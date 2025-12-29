8865cb44d13d0d76a77191a9b46a204ebf0db3dc
Sam Davidoff <sam@mfbtech.com>
2025-12-29 23:58:52 +0000

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod vec3;
 use vec3::Vec3;
 
 fn main() {
-    let vec3: Vec3<f32> = Vec3::new() ;
+    let vec3: Vec3<f32> = Vec3::new();
     println!("{} {} {}", vec3.x, vec3.y, vec3.z);
 }
-
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,32 +1,33 @@
 pub struct Vec3<T> {
     pub x: T,
     pub y: T,
-    pub z: T
+
+    pub z: T,
 }
 
-impl<T:Default> Vec3<T> {
+impl<T: Default> Vec3<T> {
     pub fn new() -> Self {
         Vec3 {
-            x : T::default(),
+            x: T::default(),
             y: T::default(),
-            z : T::default()
+            z: T::default(),
         }
     }
 }
 
-impl<T:Copy> From<T> for Vec3<T> {
-    fn from(val:T) -> Self {
+impl<T: Copy> From<T> for Vec3<T> {
+    fn from(val: T) -> Self {
         Vec3 {
             x: val,
             y: val,
-            z: val
+            z: val,
         }
     }
 }
 
-impl<T> From<(T,T,T,)> for Vec3<T> {
-    fn from((x,y,z):(T,T,T)) -> Self {
-        Vec3 {x,y,z }
+impl<T> From<(T, T, T)> for Vec3<T> {
+    fn from((x, y, z): (T, T, T)) -> Self {
+        Vec3 { x, y, z }
     }
 }
 
@@ -36,25 +37,25 @@ mod tests {
 
     #[test]
     fn create_zero_vec() {
-        let vec3:Vec3<f32> = Vec3::new();
-        assert_eq!(vec3.x,0.0);
-        assert_eq!(vec3.y,0.0);
-        assert_eq!(vec3.z,0.0);
+        let vec3: Vec3<f32> = Vec3::new();
+        assert_eq!(vec3.x, 0.0);
+        assert_eq!(vec3.y, 0.0);
+        assert_eq!(vec3.z, 0.0);
     }
 
     #[test]
     fn create_broadcast_vec() {
-        let vec3:Vec3<u16> = Vec3::from(11);
-        assert_eq!(vec3.x,11);
-        assert_eq!(vec3.y,11);
-        assert_eq!(vec3.z,11);
+        let vec3: Vec3<u16> = Vec3::from(11);
+        assert_eq!(vec3.x, 11);
+        assert_eq!(vec3.y, 11);
+        assert_eq!(vec3.z, 11);
     }
-    
+
     #[test]
     fn create_full_vec() {
-        let vec3:Vec3<u16> = Vec3::from((1,2,3));
-        assert_eq!(vec3.x,1);
-        assert_eq!(vec3.y,2);
-        assert_eq!(vec3.z,3);
+        let vec3: Vec3<u16> = Vec3::from((1, 2, 3));
+        assert_eq!(vec3.x, 1);
+        assert_eq!(vec3.y, 2);
+        assert_eq!(vec3.z, 3);
     }
 }
